e1b3e0c549f4d020d7da768fb97d5e0835ee2127
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-28 14:00:41 +0300

    Add files via upload

---
+++ CupSwapping/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CupSwapping
+{
+    class Program
+    {
+        public static string cupSwapping(string[] swaps)
+        {
+            string current = "B";
+            for(int i = 0; i < swaps.Length; i++)
+            {
+                if(swaps[i].Contains(current))
+                {
+                    current = swaps[i].Replace(current, "");
+                }
+
+            }
+            
+            return current;
+        }
+        static void Main(string[] args)
+        {
+            string[] swaps = new string[] {"CB","AC","CA"};
+            Console.WriteLine(cupSwapping(swaps));
+
+
+            Console.ReadKey();
+        }
+    }
+}
